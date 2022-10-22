831fcb30745870f4c4f56ba6c7dd4c386416f5a4
Tom Brand <tom025@me.com>
2022-10-22 20:17:05 +0100

    Kotlin: Improve name of interface

--- lang/kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ lang/kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -25,20 +25,20 @@ class GildedRose(private val items: List<Item>) {
 private fun incrementQuality(item: Item, by: Int = 1): Int = min(item.quality + by, 50)
 private fun decrementQuality(item: Item, by: Int = 1): Int = max(item.quality - by, 0)
 
-class LegendaryItem : Updatable {
+class LegendaryItem : ItemUpdater {
     override fun updateQuality(item: Item) = item.quality
 
     override fun updateSellIn(item: Item) = item.sellIn
 }
 
-class AgedBrie : Updatable {
+class AgedBrie : ItemUpdater {
     override fun updateQuality(item: Item): Int = when (item.sellIn) {
         in 1..Int.MAX_VALUE -> incrementQuality(item, by = 1)
         else -> incrementQuality(item, by = 2)
     }
 }
 
-class BackstagePasses : Updatable {
+class BackstagePasses : ItemUpdater {
     override fun updateQuality(item: Item): Int = when (item.sellIn) {
         in (11..Int.MAX_VALUE) -> incrementQuality(item)
         in (6..10) -> incrementQuality(item, by = 2)
@@ -47,7 +47,7 @@ class BackstagePasses : Updatable {
     }
 }
 
-class NormalItem : Updatable {
+class NormalItem : ItemUpdater {
     override fun updateQuality(item: Item): Int {
         return when (item.sellIn) {
             in (1..Int.MAX_VALUE) -> decrementQuality(item)
@@ -56,7 +56,7 @@ class NormalItem : Updatable {
     }
 }
 
-interface Updatable {
+interface ItemUpdater {
     fun updateQuality(item: Item): Int
     fun updateSellIn(item: Item): Int = item.sellIn - 1
 }
