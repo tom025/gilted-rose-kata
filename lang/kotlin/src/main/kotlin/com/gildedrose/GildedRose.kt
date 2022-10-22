bf433e5d2280993ece8ed33724f4c50a39897341
Tom Brand <tom025@me.com>
2022-10-22 19:06:41 +0100

    Kotlin: Use extension function for incrementing and decrementing items

--- lang/kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ lang/kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -14,33 +14,33 @@ class GildedRose(private val items: List<Item>) {
             }
             "Aged Brie" -> {
                 when (item.sellIn) {
-                    in 1..Int.MAX_VALUE -> incrementQuality(item, by = 1)
-                    else -> incrementQuality(item, by = 2)
+                    in 1..Int.MAX_VALUE -> item.incrementQuality(by = 1)
+                    else -> item.incrementQuality(by = 2)
                 }
             }
             "Backstage passes to a TAFKAL80ETC concert" -> {
                 when (item.sellIn) {
-                    in (11..Int.MAX_VALUE) -> incrementQuality(item)
-                    in (6..10) -> incrementQuality(item, by = 2)
-                    in (1..5) -> incrementQuality(item, by = 3)
+                    in (11..Int.MAX_VALUE) -> item.incrementQuality()
+                    in (6..10) -> item.incrementQuality(by = 2)
+                    in (1..5) -> item.incrementQuality(by = 3)
                     else -> item.quality = 0
                 }
             }
             else -> {
                 when (item.sellIn) {
-                    in (1..Int.MAX_VALUE) -> decrementQuality(item)
-                    else -> decrementQuality(item, by = 2)
+                    in (1..Int.MAX_VALUE) -> item.decrementQuality()
+                    else -> item.decrementQuality(by = 2)
                 }
             }
         }
         if (item.name != "Sulfuras, Hand of Ragnaros") item.sellIn = item.sellIn - 1
     }
 
-    private fun decrementQuality(item: Item, by: Int = 1) {
-        item.quality = max(item.quality - by, 0)
+    private fun Item.decrementQuality(by: Int = 1) {
+        quality = max(quality - by, 0)
     }
 
-    private fun incrementQuality(item: Item, by: Int = 1) {
-        item.quality = min(item.quality + by, 50)
+    private fun Item.incrementQuality(by: Int = 1) {
+        quality = min(quality + by, 50)
     }
 }
