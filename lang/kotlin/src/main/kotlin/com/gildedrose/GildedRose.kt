d153a5196b1236a68f686cf9a553bae54bc43e27
Tom Brand <tom025@me.com>
2022-10-22 19:29:36 +0100

    Kotlin: Introduce polymorphism

--- lang/kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ lang/kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -10,37 +10,67 @@ class GildedRose(private val items: List<Item>) {
 
     private fun updateItem(item: Item) {
         when (item.name) {
-            "Sulfuras, Hand of Ragnaros" -> { /* Do nothing */
-            }
-            "Aged Brie" -> {
-                when (item.sellIn) {
-                    in 1..Int.MAX_VALUE -> item.incrementQuality(by = 1)
-                    else -> item.incrementQuality(by = 2)
-                }
-            }
-            "Backstage passes to a TAFKAL80ETC concert" -> {
-                when (item.sellIn) {
-                    in (11..Int.MAX_VALUE) -> item.incrementQuality()
-                    in (6..10) -> item.incrementQuality(by = 2)
-                    in (1..5) -> item.incrementQuality(by = 3)
-                    else -> item.quality = 0
-                }
-            }
-            else -> {
-                when (item.sellIn) {
-                    in (1..Int.MAX_VALUE) -> item.decrementQuality()
-                    else -> item.decrementQuality(by = 2)
-                }
-            }
+            "Sulfuras, Hand of Ragnaros" -> LegendaryItem(item)
+            "Aged Brie" -> AgedBrie(item)
+            "Backstage passes to a TAFKAL80ETC concert" -> BackstagePasses(item)
+            else -> NormalItem(item)
+        }.apply {
+            updateQuality()
+            updateSellIn()
         }
-        if (item.name != "Sulfuras, Hand of Ragnaros") item.sellIn = item.sellIn - 1
     }
 
-    private fun Item.decrementQuality(by: Int = 1) {
-        quality = max(quality - by, 0)
+}
+
+private fun Item.decrementQuality(by: Int = 1) {
+    quality = max(quality - by, 0)
+}
+
+private fun Item.incrementQuality(by: Int = 1) {
+    quality = min(quality + by, 50)
+}
+
+class LegendaryItem(override val item: Item) : Updatable {
+    override fun updateQuality() { /* Do nothing */
     }
 
-    private fun Item.incrementQuality(by: Int = 1) {
-        quality = min(quality + by, 50)
+    override fun updateSellIn() { /* Do nothing */
+    }
+}
+
+class AgedBrie(override val item: Item) : Updatable {
+    override fun updateQuality() {
+        when (item.sellIn) {
+            in 1..Int.MAX_VALUE -> item.incrementQuality(by = 1)
+            else -> item.incrementQuality(by = 2)
+        }
+    }
+}
+
+class BackstagePasses(override val item: Item) : Updatable {
+    override fun updateQuality() {
+        when (item.sellIn) {
+            in (11..Int.MAX_VALUE) -> item.incrementQuality()
+            in (6..10) -> item.incrementQuality(by = 2)
+            in (1..5) -> item.incrementQuality(by = 3)
+            else -> item.quality = 0
+        }
+    }
+}
+
+class NormalItem(override val item: Item) : Updatable {
+    override fun updateQuality() {
+        when (item.sellIn) {
+            in (1..Int.MAX_VALUE) -> item.decrementQuality()
+            else -> item.decrementQuality(by = 2)
+        }
+    }
+}
+
+interface Updatable {
+    val item: Item
+    fun updateQuality()
+    fun updateSellIn() {
+        item.sellIn = item.sellIn - 1
     }
 }
